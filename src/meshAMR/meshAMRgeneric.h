5f9dccac00ca51325898b508ed5de81a83386724
vtenishe <vtenishe@srbwks2014-0079.engin.umich.edu>
2018-07-05 14:24:15 -0400

    modified CreateNewParallelDistributionLists(): the load splitting scheme was tuned up

--- src/meshAMR/meshAMRgeneric.h
+++ src/meshAMR/meshAMRgeneric.h
@@ -9540,6 +9540,8 @@ nMPIops++;
         }
 
         startNode->ParallelLoadMeasure=res;
+//        printf("block xCenter:%e,%e,%e, block load:%e\n",0.5*(startNode->xmin[0]+startNode->xmax[0]),0.5*(startNode->xmin[1]+startNode->xmax[1]),0.5*(startNode->xmin[2]+startNode->xmax[2]),startNode->ParallelLoadMeasure);
+
       }
       else pipe.send(res);
       #elif _AMR_PARALLEL_MODE_ == _AMR_PARALLEL_MODE_OFF_
@@ -10081,7 +10083,6 @@ if (TmpAllocationCounter==2437) {
 
       while (CurveNode!=NULL) {
         CumulativeProcessorLoad+=CurveNode->ParallelLoadMeasure;
-        CumulativeThreadLoad[nCurrentProcessorBalancing]+=CurveNode->ParallelLoadMeasure;
 
         if ((isfinite(CumulativeProcessorLoad)==false)||(isfinite(CumulativeThreadLoad[nCurrentProcessorBalancing])==false)) {
           char msg[500];
@@ -10091,11 +10092,12 @@ if (TmpAllocationCounter==2437) {
         }  
 
         //increment the processor number if needed
-        if ((CumulativeProcessorLoad>1.0+nCurrentProcessorBalancing)&&(nCurrentProcessorBalancing!=nTotalThreads-1)) {
+        if ((CumulativeProcessorLoad>1.1+nCurrentProcessorBalancing)&&(nCurrentProcessorBalancing!=nTotalThreads-1)&&(CumulativeThreadLoad[nCurrentProcessorBalancing]>0.0)) {
           nCurrentProcessorBalancing++;
-          ThreadStartNode[nCurrentProcessorBalancing]=CurveNode->FillingCurveNextNode;
+          ThreadStartNode[nCurrentProcessorBalancing]=CurveNode;
         }
 
+        CumulativeThreadLoad[nCurrentProcessorBalancing]+=CurveNode->ParallelLoadMeasure;
         CurveNode=CurveNode->FillingCurveNextNode;
       }
 
