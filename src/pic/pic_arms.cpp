718eb69b0222de5937fcdcdd30c2bcb0a564aede
Tenishev <vtenishe@csrwks2018-0093.engin.umich.edu>
2018-08-09 15:54:47 -0400

    'std::isnan(time)' has been replaced with 'isnan(time)' to make Intel compiler happy

--- src/pic/pic_arms.cpp
+++ src/pic/pic_arms.cpp
@@ -46,7 +46,7 @@ double PIC::CPLR::DATAFILE::ARMS::GetFileTime(const char *fname){
   }
 
   fin.closefile();
-  if (std::isnan(time)) exit(__LINE__, __FILE__,"ERROR: failed to read valid time from ARMS datafile"); 
+  if (isnan(time)) exit(__LINE__, __FILE__,"ERROR: failed to read valid time from ARMS datafile"); 
   return time;
 }
 
