e7c686ab2d6c151ea9be9863d100b2224f037459
Valeriy Tenishev <vtenishe@pfe20.nas.nasa.gov>
2019-10-04 14:49:08 -0700

    the list of the tests is extended

--- utility/TestScripts/CheckTests.pl
+++ utility/TestScripts/CheckTests.pl
@@ -7,7 +7,7 @@ my @TestTable=("test_batl-reader.diff/test_batl-reader_check",
 "test_binary-reader.diff/test_binary-reader_check", 
 "test_CCMC.diff/test_CCMC_check", 
 "test_CCMC-Individual_Trajectories.diff/test_CCMC-Individual_Trajectories_check", 
-"test_CG[?].diff/test_CG_check", 
+"test_CG\\[?\\].diff/test_CG_check", 
 "test_CG-BATSRUS-dust-coupling.diff/test_CG-BATSRUS-dust-coupling_check", 
 "test_CG-dust-test\\[?\\].diff/test_CG-dust-test_check", 
 "test_COPS.diff/test_COPS_check", 
@@ -16,7 +16,14 @@ my @TestTable=("test_batl-reader.diff/test_batl-reader_check",
 "test_fast-wave.diff/test_fast-wave_check", 
 "test_fast-wave-openmp.diff/test_fast-wave-openmp_check", 
 "test_lightwave-current-off.diff/test_lightwave-current-off_check", 
-"test_lightwave-current-off-openmp.diff/test_lightwave-current-off-openmp_check");  
+"test_lightwave-current-off-openmp.diff/test_lightwave-current-off-openmp_check",
+"test_photon-test.diff/test_photon-test_check",
+"test_X-Wing.diff/test_X-Wing_check",
+"test_Moon.diff/test_Moon_check",
+"test_Individual_Trajectories--Boris\\[?\\].diff/test_Individual_Trajectories--Boris_check",
+"test_Individual_Trajectories--Boris-relativistic\\[?\\].diff/test_Individual_Trajectories--Boris-relativistic_check",
+"test_Individual_Trajectories--Markidis2010\\[?\\].diff/test_Individual_Trajectories--Markidis2010_check",
+"test_Individual_Trajectories--guiding-center\\[?\\].diff/test_Individual_Trajectories--guiding-center_check");  
 
 my ($fname,$target,$pair,$size,$cmd);
 
