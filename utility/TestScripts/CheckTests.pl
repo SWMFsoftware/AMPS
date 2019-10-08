35c3a910830846664a6b724b032842fbdc0bf6de
Valeriy Tenishev <vtenishe@login2.stampede2.tacc.utexas.edu>
2019-10-08 10:31:14 -0500

    the list of the tests was appended

--- utility/TestScripts/CheckTests.pl
+++ utility/TestScripts/CheckTests.pl
@@ -32,7 +32,13 @@ my @TestTable=("test_batl-reader.diff/test_batl-reader_check",
 "test_InterpolateAMR-AMPS-CornerBased-test.diff/test_InterpolateAMR-AMPS-test_check", 
 "test_InterpolateAMR-AMPS-test/test_InterpolateAMR-AMPS-test_check",
 "test_Periodic-BC-NonUniform.diff/test_Periodic-BC-NonUniform_check",
-"test_Mars-ions.diff/test_Mars-ions_check"); 
+"test_Mars-ions.diff/test_Mars-ions_check",
+"test_Saturn-MOP\\[?\\].diff/test_Saturn-MOP_check",
+"test_Mover-Boris.diff/test_Mover-Boris_check",
+"test_Mover-Boris\\[?\\]/test_Mover-Boris_check",
+"test_Mover-Boris-relativistic.diff/test_Mover-Boris-relativistic_check",
+"test_Mover-Boris-relativistic\\[?\\].diff/test_Mover-Boris-relativistic_check",
+"test_Chemistry-Test.diff/test_Chemistry-Test_check"); 
 
 my ($fname,$target,$pair,$size,$cmd);
 
