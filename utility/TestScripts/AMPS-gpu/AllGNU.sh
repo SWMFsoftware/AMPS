666763fcec02369e319b10f0dcdacb3ea288f59c
Valeriy M Tenishev <vtenishe@umich.edu>
2020-01-03 16:53:40 -0500

    the default number of the MPI processes to run the tests is set to 8 to use reference solutions calculated for other Linux mechines used in nightly testing of AMPS

--- utility/TestScripts/AMPS-gpu/AllGNU.sh
+++ utility/TestScripts/AMPS-gpu/AllGNU.sh
@@ -13,5 +13,5 @@ cd $WorkDir/Tmp_AMPS_test/GNU/AMPS/
 echo " done."
 
 echo -n "Executing tests GNU....."
-make TESTMPIRUN4="mpirun -np 4"  MPIRUN="mpirun -np 4" TESTMPIRUN1="mpirun -np 1" test_run >>& test_amps.log
+make TESTMPIRUN4="mpirun -np 4"  MPIRUN="mpirun -np 8" TESTMPIRUN1="mpirun -np 1" test_run >>& test_amps.log
 echo " done."
