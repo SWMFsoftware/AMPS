316b763e83a6cd1d47623e32bd50e5d080f9ab4b
Valeriy M Tenishev <vtenishe@umich.edu>
2020-01-27 18:17:16 -0500

    file 'AmpsTestGNUComplete' is created when the test routine is completed

--- utility/TestScripts/AMPS-gpu/AllGNU.sh
+++ utility/TestScripts/AMPS-gpu/AllGNU.sh
@@ -14,4 +14,8 @@ echo " done."
 
 echo -n "Executing tests GNU....."
 make TESTMPIRUN4="mpirun -np 4"  MPIRUN="mpirun -np 8" TESTMPIRUN1="mpirun -np 1" test_run >>& test_amps.log
+
 echo " done."
+
+cd $WorkDir/Tmp_AMPS_test
+echo Done > AmpsTestGNUComplete
