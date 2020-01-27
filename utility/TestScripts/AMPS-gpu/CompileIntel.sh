aac8396e7208e1f417d3a335001ecb343850158b
Valeriy M Tenishev <vtenishe@umich.edu>
2020-01-27 18:18:20 -0500

    compile tests with Intel compiler

---
+++ utility/TestScripts/AMPS-gpu/CompileIntel.sh
@@ -0,0 +1,14 @@
+#!/bin/csh
+
+set WorkDir = $HOME
+source $WorkDir/module/intel 
+
+echo -n "Compiling Intel....."
+
+cd $WorkDir/Tmp_AMPS_test/Intel/AMPS; 
+make test_compile >>& test_amps.log
+
+echo " done."
+
+cd $WorkDir/Tmp_AMPS_test
+echo Done > AmpsCompilingIntelComplete
