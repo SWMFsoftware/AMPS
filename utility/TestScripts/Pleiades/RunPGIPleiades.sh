05c8fd2eaedfbc27726d30c8f104fdcb396756d2
Valeriy Tenishev <vtenishe@pfe24.nas.nasa.gov>
2020-01-28 13:27:01 -0800

    the list of the loaded modules is updated

--- utility/TestScripts/Pleiades/RunPGIPleiades.sh
+++ utility/TestScripts/Pleiades/RunPGIPleiades.sh
@@ -2,8 +2,9 @@
 
 cd PGI/AMPS
 module purge
-module load comp-pgi/17.1
+module load comp-pgi/19.10 
 module load mpi-sgi/mpt
+module load mpi-hpe/mpt.2.17r13
 module load tecplot/2017r2
 make $1 MPIRUN="mpiexec -n 8 dplace -s1 -c 8-15" >>& test_amps.log
 
