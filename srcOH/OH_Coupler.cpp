58f6f4416f207d7b709e4eaee8a6ccb07c6d9f39
Valeriy M Tenishev <vtenishe@umich.edu>
2020-05-16 11:42:56 -0400

    the coupler was rolled back to the previous version where the sum of souces (source+loss) were passed into the coupled instead of separated source and loss terms

--- srcOH/OH_Coupler.cpp
+++ srcOH/OH_Coupler.cpp
@@ -14,23 +14,10 @@ void OH::Coupling::Send(char *NameVar, int *nVarIn, int *nDimIn, int *nPoint, do
   int i0=0,i1=0;
   char vname[200];
 
-  int density_source_offset=0;
-  int momentum_source_offset=1;
-  int energy_source_offset=4;
-
-  int density_sink_offset=0;
-  int momentum_sink_offset=1;
-  int energy_sink_offset=4;
-
+  int density_offset=0;
+  int momentum_offset=1;
+  int energy_offset=4;
   int package_length=5;
-
-/*
-  int density_sink_offset=5;
-  int momentum_sink_offset=6;
-  int energy_sink_offset=9;
-
-  int package_length=10;
-*/
 
   //save the data
   cTreeNodeAMR<PIC::Mesh::cDataBlockAMR>* node=NULL;
@@ -57,33 +44,29 @@ void OH::Coupling::Send(char *NameVar, int *nVarIn, int *nDimIn, int *nPoint, do
     for (ifluid=0;ifluid<PIC::CPLR::SWMF::nCommunicatedIonFluids;ifluid++) {
       if (PIC::LastSampleLength!=0) {
         // copy density source
-        Data_VI[fluid_data_offset+density_source_offset+pt*(*nVarIn)]=
+        Data_VI[fluid_data_offset+density_offset+pt*(*nVarIn)]=
           (*(ifluid+(double*)(AssociatedDataPointer+PIC::Mesh::completedCellSampleDataPointerOffset+OH::Output::ohSourceDensityOffset)))/PIC::LastSampleLength;
 
         // copy momentum source
-        for (int idim=0;idim<3;idim++) Data_VI[fluid_data_offset+idim+momentum_source_offset+pt*(*nVarIn)]=
-          (*(idim+3*ifluid+(double*)(AssociatedDataPointer+PIC::Mesh::completedCellSampleDataPointerOffset+OH::Output::ohSourceMomentumOffset)))/PIC::LastSampleLength;
+        Data_VI[fluid_data_offset+0+momentum_offset+pt*(*nVarIn)]=
+          (*(0+3*ifluid+(double*)(AssociatedDataPointer+PIC::Mesh::completedCellSampleDataPointerOffset+OH::Output::ohSourceMomentumOffset)))/PIC::LastSampleLength;
+
+        Data_VI[fluid_data_offset+1+momentum_offset+pt*(*nVarIn)]=
+          (*(1+3*ifluid+(double*)(AssociatedDataPointer+PIC::Mesh::completedCellSampleDataPointerOffset+OH::Output::ohSourceMomentumOffset)))/PIC::LastSampleLength;
+
+        Data_VI[fluid_data_offset+2+momentum_offset+pt*(*nVarIn)]=
+          (*(2+3*ifluid+(double*)(AssociatedDataPointer+PIC::Mesh::completedCellSampleDataPointerOffset+OH::Output::ohSourceMomentumOffset)))/PIC::LastSampleLength;
 
         // copy energy source
-        Data_VI[fluid_data_offset+energy_source_offset+pt*(*nVarIn)]=
+        Data_VI[fluid_data_offset+energy_offset+pt*(*nVarIn)]=
           (*(ifluid+(double*)(AssociatedDataPointer+PIC::Mesh::completedCellSampleDataPointerOffset+OH::Output::ohSourceEnergyOffset)))/PIC::LastSampleLength;
-
-
-        // copy density sink 
-        Data_VI[fluid_data_offset+density_sink_offset+pt*(*nVarIn)]-=
-          (*(ifluid+(double*)(AssociatedDataPointer+PIC::Mesh::completedCellSampleDataPointerOffset+OH::Output::ohSinkDensityOffset)))/PIC::LastSampleLength;
-
-        // copy momentum sink 
-        for (int idim=0;idim<3;idim++) Data_VI[fluid_data_offset+idim+momentum_sink_offset+pt*(*nVarIn)]-=
-          (*(idim+3*ifluid+(double*)(AssociatedDataPointer+PIC::Mesh::completedCellSampleDataPointerOffset+OH::Output::ohSinkMomentumOffset)))/PIC::LastSampleLength;
-
-        // copy energy sink 
-        Data_VI[fluid_data_offset+energy_sink_offset+pt*(*nVarIn)]-=
-          (*(ifluid+(double*)(AssociatedDataPointer+PIC::Mesh::completedCellSampleDataPointerOffset+OH::Output::ohSinkEnergyOffset)))/PIC::LastSampleLength;
-
       }
       else {
-        for (int offset=0;offset<package_length;offset++) Data_VI[fluid_data_offset+offset+pt*(*nVarIn)]=0.0; 
+        Data_VI[fluid_data_offset+density_offset  +pt*(*nVarIn)]=0.0;
+        Data_VI[fluid_data_offset+0+momentum_offset+pt*(*nVarIn)]=0.0;
+        Data_VI[fluid_data_offset+1+momentum_offset+pt*(*nVarIn)]=0.0;
+        Data_VI[fluid_data_offset+2+momentum_offset+pt*(*nVarIn)]=0.0;
+        Data_VI[fluid_data_offset+energy_offset   +pt*(*nVarIn)]=0.0;
       }
 
       fluid_data_offset+=package_length;
