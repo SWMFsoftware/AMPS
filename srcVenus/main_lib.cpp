ba955d50fe4d28a22c461c473e3f2b83e16c13cc
Valeriy M Tenishev <vtenishe@umich.edu>
2020-09-02 13:54:29 -0400

    added: default functions from namespace Exosphere

--- srcVenus/main_lib.cpp
+++ srcVenus/main_lib.cpp
@@ -1,2 +1,16 @@
-$Id$
 //this hile is a placeholder. It should contain the initialization functions that are currently in main.cpp, which is needed to make the model comparible the SWMF
+
+#include "pic.h"
+#include "constants.h"
+
+#include "Exosphere.h"
+
+//default definition of the functions for the exosphere module
+double Exosphere::OrbitalMotion::GetTAA(SpiceDouble et) {return 0.0;}
+int Exosphere::ColumnIntegral::GetVariableList(char *vlist) {return 0;}
+void Exosphere::ColumnIntegral::ProcessColumnIntegrationVector(double *res,int resLength) {}
+double Exosphere::GetSurfaceTemeprature(double cosSubsolarAngle,double *x_LOCAL_SO_OBJECT) {return 0.0;}
+char Exosphere::SO_FRAME[_MAX_STRING_LENGTH_PIC_]="GALL_EPHIOD";
+char Exosphere::ObjectName[_MAX_STRING_LENGTH_PIC_]="Europa";
+void Exosphere::ColumnIntegral::CoulumnDensityIntegrant(double *res,int resLength,double* x,cTreeNodeAMR<PIC::Mesh::cDataBlockAMR>* node) {}
+double Exosphere::SurfaceInteraction::StickingProbability(int spec,double& ReemissionParticleFraction,double Temp) {return 0.0;}
