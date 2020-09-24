14b53ee87ef63fa51682398552937078bd4cb533
Tenishev <vtenishe@csrwks2018-0093.engin.umich.edu>
2020-09-24 16:19:30 -0400

    added: scripts for separated compiling and execution of tests on amps.engin.umich.edu. The purpose is to accelerate execution of the tests as when running on amps-gpu.engin.umich.edu.

---
+++ utility/TestScripts/Valeriy/RunGNU.sh
@@ -0,0 +1,20 @@
+#!/bin/csh
+
+set WorkDir = $HOME
+source $WorkDir/module/gnu 
+
+echo -n "Executing tests GNU....."
+
+cd $WorkDir/Tmp_AMPS_test/GNU/AMPS/
+
+if ($#argv != 1) then
+  make TESTMPIRUN4="mpirun -np 4"  MPIRUN="mpirun -np 4" TESTMPIRUN1="export DYLD_LIBRARY_PATH=/Users/ccmc/boost/lib:/opt/intel/compilers_and_libraries/mac/lib;mpirun -np 1" test_run >>& test_amps.log
+else 
+  echo Starting test_run_thread$1
+  make TESTMPIRUN4="mpirun -np 4"  MPIRUN="mpirun -np 4" TESTMPIRUN1="export DYLD_LIBRARY_PATH=/Users/ccmc/boost/lib:/opt/intel/compilers_and_libraries/mac/lib;mpirun -np 1" test_run_thread$1 >>& test_amps$1.log
+endif
+
+echo " done."
+
+cd $WorkDir/Tmp_AMPS_test
+echo Done > AmpsTestGNUComplete
---
+++ utility/TestScripts/Valeriy/RunIntel.sh
@@ -0,0 +1,20 @@
+#!/bin/csh
+
+set WorkDir = $HOME
+source $WorkDir/module/intel 
+
+
+cd $WorkDir/Tmp_AMPS_test/Intel/AMPS; 
+
+echo -n "Executing tests Intel....."
+
+if ($#argv != 1) then
+  make TESTMPIRUN4="mpirun -np 4" MPIRUN="export DYLD_LIBRARY_PATH=/Users/ccmc/boost/lib:/opt/intel/compilers_and_libraries/mac/lib;mpirun -np 4" TESTMPIRUN1="export DYLD_LIBRARY_PATH=/Users/ccmc/boost/lib:/opt/intel/compilers_and_libraries/mac/lib;mpirun -np 1" test_run >>& test_amps.log
+else 
+  make TESTMPIRUN4="mpirun -np 4" MPIRUN="export DYLD_LIBRARY_PATH=/Users/ccmc/boost/lib:/opt/intel/compilers_and_libraries/mac/lib;mpirun -np 4" TESTMPIRUN1="export DYLD_LIBRARY_PATH=/Users/ccmc/boost/lib:/opt/intel/compilers_and_libraries/mac/lib;mpirun -np 1" test_run_thread$1 >>& test_amps$1.log
+endif
+
+echo " done."
+
+cd $WorkDir/Tmp_AMPS_test
+echo Done > AmpsTestIntelComplete
