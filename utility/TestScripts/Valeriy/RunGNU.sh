7178f3fd2829cc8190de7a07010f1fd93e5f74c2
Tenishev <vtenishe@csrwks2018-0093.engin.umich.edu>
2020-09-27 12:20:27 -0400

    modified RunGNU.sh RunIntel.sh: in case the scripts are executed when the scheduler is used in the multipricess regime, creating ftime AmpsTestIntelComplete and AmpsTestGNUComplete are not needed.

--- utility/TestScripts/Valeriy/RunGNU.sh
+++ utility/TestScripts/Valeriy/RunGNU.sh
@@ -9,6 +9,9 @@ cd $WorkDir/Tmp_AMPS_test/GNU/AMPS/
 
 if ($#argv != 1) then
   make TESTMPIRUN4="mpirun -np 4"  MPIRUN="mpirun -np 4" TESTMPIRUN1="export DYLD_LIBRARY_PATH=/Users/ccmc/boost/lib:/opt/intel/compilers_and_libraries/mac/lib;mpirun -np 1" test_run >>& test_amps.log
+
+  cd $WorkDir/Tmp_AMPS_test
+  echo Done > AmpsTestGNUComplete
 else 
   echo Starting test_run_thread$1
   make TESTMPIRUN4="mpirun -np 4"  MPIRUN="mpirun -np 4" TESTMPIRUN1="export DYLD_LIBRARY_PATH=/Users/ccmc/boost/lib:/opt/intel/compilers_and_libraries/mac/lib;mpirun -np 1" test_run_thread$1 >& test_amps$1.log
@@ -16,5 +19,3 @@ endif
 
 echo " done."
 
-cd $WorkDir/Tmp_AMPS_test
-echo Done > AmpsTestGNUComplete
