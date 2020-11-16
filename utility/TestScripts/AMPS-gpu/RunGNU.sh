a55574c80aeaf3cac24e051a37c20bdd89c7ab97
Valeriy M Tenishev <vtenishe@umich.edu>
2020-11-16 00:37:06 -0500

    script RunGNU.sh is corrected

--- utility/TestScripts/AMPS-gpu/RunGNU.sh
+++ utility/TestScripts/AMPS-gpu/RunGNU.sh
@@ -14,7 +14,7 @@ if ($#argv != 1) then
 else
   echo Starting test_run_thread$1
   make TESTMPIRUN4="mpirun -np 4"  MPIRUN="mpirun -np 8" TESTMPIRUN1="mpirun -np 1" test_run_thread$1 >>& test_amps_thread$1.log
-  echo  done (test_run_thread$1).
+  echo  done test_run_thread$1.
 endif
 
 cd $WorkDir/Tmp_AMPS_test
