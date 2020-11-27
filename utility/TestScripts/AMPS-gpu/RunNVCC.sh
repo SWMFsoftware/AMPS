5c7482df5ad998737cfa6ff5aa16e5d1aa69bb70
Valeriy M Tenishev <vtenishe@umich.edu>
2020-11-27 16:42:44 -0500

    minor correction in the script

--- utility/TestScripts/AMPS-gpu/RunNVCC.sh
+++ utility/TestScripts/AMPS-gpu/RunNVCC.sh
@@ -1,22 +1,21 @@
 #!/bin/csh
-
-source /etc/csh/login.d/env.csh
+module load mpi
 
 set WorkDir = $HOME
 source $WorkDir/module/gnu 
 
 echo -n "Executing tests NVCC....."
 
-cd $WorkDir/Tmp_AMPS_test/NVCC/AMPS/
+cd $WorkDir/Tmp_AMPS_test/NVCC/AMPS  
 
 if ($#argv != 1) then
   make TESTMPIRUN4="mpirun -np 4"  MPIRUN="mpirun -np 8" TESTMPIRUN1="mpirun -np 1" test_run >>& test_amps.log
+  echo " done."
 else
   echo Starting test_run_thread$1
   make TESTMPIRUN4="mpirun -np 4"  MPIRUN="mpirun -np 8" TESTMPIRUN1="mpirun -np 1" test_run_thread$1 >>& test_amps_thread$1.log
+  echo  done test_run_thread$1.
 endif
-
-echo " done."
 
 cd $WorkDir/Tmp_AMPS_test
 echo Done > AmpsTestNVCCComplete
