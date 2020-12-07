39335c833c6a49c0b20288902dc2f25dc0119ed5
Valeriy M Tenishev <vtenishe@umich.edu>
2020-12-07 11:57:38 -0500

    added: kernel_5 and kernel_6

--- src/general/specfunc.h
+++ src/general/specfunc.h
@@ -893,6 +893,19 @@ void kernel_4(F f, T1 t1,T2 t2,T3 t3,T4 t4) {
   f(t1,t2,t3,t4);
 } 
 
+template <class F,class T1, class T2,class T3,class T4,class T5>
+__global__
+void kernel_5(F f, T1 t1,T2 t2,T3 t3,T4 t4,T5 t5) {
+  f(t1,t2,t3,t4,t5);
+}
+
+template <class F,class T1, class T2,class T3,class T4,class T5,class T6>
+__global__
+void kernel_6(F f, T1 t1,T2 t2,T3 t3,T4 t4,T5 t5,T6 t6) {
+  f(t1,t2,t3,t4,t5,t6);
+}
+
+
 #endif
 
 #endif
