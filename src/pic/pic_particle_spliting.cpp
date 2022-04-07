7feb0964da5b69408e045db1c08a9a2f1e683ef8
Valeriy M Tenishev <vtenishe@umich.edu>
2022-04-07 00:30:56 -0400

    PIC::ParticleSplitting::Split::Scatter(): The function is redone. The algorithm for selecting particles to be deleted is the same as in PIC::ParticleSplitting::Split::SplitWithVelocityShift(). The fucntion produces reasonable results when used for SWMF's test14_1d.

--- src/pic/pic_particle_spliting.cpp
+++ src/pic/pic_particle_spliting.cpp
@@ -211,21 +211,21 @@ void PIC::ParticleSplitting::Split::Scatter(int particle_num_limit_min,int parti
   }
 
   //get the number of model particle in the cell block 2x2x2 started from position i0,j0,k0
-  auto GetParticleNumber = [] (int i,int j,int k,cTreeNodeAMR<PIC::Mesh::cDataBlockAMR> *node)  {
+  auto GetParticleNumber = [] (int spec,int i,int j,int k,cTreeNodeAMR<PIC::Mesh::cDataBlockAMR> *node)  {
     int ParticleNumber=0;
     long int p;
 
     p=node->block->FirstCellParticleTable[i+_BLOCK_CELLS_X_*(j+_BLOCK_CELLS_Y_*k)];
 
     while (p!=-1) {
-      ParticleNumber++;
+      if (PIC::ParticleBuffer::GetI(p)==spec) ParticleNumber++;
       p=PIC::ParticleBuffer::GetNext(p);
     }
 
     return ParticleNumber;
   };
   
-  auto GetBlockParticleNumber = [&] (int i0,int j0,int k0,cTreeNodeAMR<PIC::Mesh::cDataBlockAMR> *node)  {
+  auto GetBlockParticleNumber = [&] (int spec,int i0,int j0,int k0,cTreeNodeAMR<PIC::Mesh::cDataBlockAMR> *node)  {
     int di,dj,dk,i,j,k;
     int ParticleNumber=0;
     
@@ -234,7 +234,7 @@ void PIC::ParticleSplitting::Split::Scatter(int particle_num_limit_min,int parti
       j=j0+dj;
       k=k0+dk; 
       
-      ParticleNumber+=GetParticleNumber(i,j,k,node);
+      ParticleNumber+=GetParticleNumber(spec,i,j,k,node);
     }
     
     return ParticleNumber;
@@ -253,87 +253,79 @@ void PIC::ParticleSplitting::Split::Scatter(int particle_num_limit_min,int parti
     int nd;
   };
   
-  vector<cParticleListElement>  ParticleList;
-  
-  //reduec the number of model particles 
-  auto ReduceParicleNumber = [&] (int i,int j,int k,cTreeNodeAMR<PIC::Mesh::cDataBlockAMR> *node,vector<cParticleListElement>& ParticleList,int nRequestedParticles)  { 
-    int nTotalParticles=0;
+  class cParticleDescriptor {
+  public:
     long int p;
-    double SummWeightCorrection=0.0,WeightCorrectionMax=0.0,w,RemovedWeightCorrectionFactor=0.0;
-    cParticleListElement t;
-    
-    ParticleList.clear();
-    p=node->block->FirstCellParticleTable[i+_BLOCK_CELLS_X_*(j+_BLOCK_CELLS_Y_*k)];
-    
-    while (p!=-1) {
-      w=PIC::ParticleBuffer::GetIndividualStatWeightCorrection(p);
-      
-      t.ptr=p;
-      t.w=w;
-      SummWeightCorrection+=w;
-      
-      if (WeightCorrectionMax<w) WeightCorrectionMax=w;
-      
-      ParticleList.push_back(t);
-      nTotalParticles++;
-      
-      p=PIC::ParticleBuffer::GetNext(p);      
-    }
-  
-    //remove particles 
-    int nRemovedParticles=nTotalParticles-nRequestedParticles;
-    
-    for (int i=0;i<nRemovedParticles;i++) {
-      int ii;
-      bool flag=false;
-
-      do {
-        ii=(int)(rnd()*nTotalParticles);
-      
-        p=ParticleList[ii].ptr;
-      
-        if (ParticleList[ii].w/WeightCorrectionMax<rnd()) { //particles with higher weight has more chanse to remain in the system
-          RemovedWeightCorrectionFactor+=ParticleList[ii].w;
-        
-          PIC::ParticleBuffer::DeleteParticle(p);
-          ParticleList[ii]=ParticleList[nTotalParticles-1];
-          nTotalParticles--;
-          flag=true;
-        }
-      }
-      while (flag==false);
-    }
-    
-    //rebuild the particle list 
-    double WeightRatio=SummWeightCorrection/(SummWeightCorrection-RemovedWeightCorrectionFactor);
-    
-    for (int i=0;i<nTotalParticles;i++) {
-      w=PIC::ParticleBuffer::GetIndividualStatWeightCorrection(p);
-      w*=WeightRatio;
-      PIC::ParticleBuffer::SetIndividualStatWeightCorrection(w,p);
-      
-      if (i==0) {
-        PIC::ParticleBuffer::SetPrev(-1,ParticleList[0].ptr);
-      }
-      else  {
-        PIC::ParticleBuffer::SetPrev(ParticleList[i-1].ptr,ParticleList[i].ptr);
-      }
-      
-      if (i==nTotalParticles-1) {
-        PIC::ParticleBuffer::SetNext(-1,ParticleList[i].ptr);
-      }
-      else {
-        PIC::ParticleBuffer::SetNext(ParticleList[i+1].ptr,ParticleList[i].ptr);
-      }
-    }
-    
-    //replace the starting point of the list 
-    node->block->FirstCellParticleTable[i+_BLOCK_CELLS_X_*(j+_BLOCK_CELLS_Y_*k)]=ParticleList[0].ptr;
+    double w;
   };
   
+  vector<cParticleListElement>  ParticleList;
   
+  //reduce the number of model particles 
+  auto ReduceParticleNumber = [&] (int spec,int i,int j,int k,cTreeNodeAMR<PIC::Mesh::cDataBlockAMR> *node,int nRequestedParticles)  {
+    vector<cParticleDescriptor> ParticleList;  
+    cParticleDescriptor t;
+    double *v,w_max=0.0,w_min=-1.0,SummedWeight=0.0,w;
+    int nModelParticles=0;
+    long int p;
+
+    p=node->block->FirstCellParticleTable[i+_BLOCK_CELLS_X_*(j+_BLOCK_CELLS_Y_*k)];
+
+    while (p!=-1) {
+      if (spec==PIC::ParticleBuffer::GetI(p)) { 
+        t.p=p;
+        t.w=PIC::ParticleBuffer::GetIndividualStatWeightCorrection(p);
+        ParticleList.push_back(t);
+
+        SummedWeight+=t.w;
+        nModelParticles++;
+
+        if (w_max<t.w) w_max=t.w;
+        if ((w_min<0.0)||(w_min>t.w)) w_min=t.w; 
+      }
+
+      p=PIC::ParticleBuffer::GetNext(p);
+    }
+
+    //delete particles 
+    int nDeleteParticles=nModelParticles-nRequestedParticles,ip; 
+    double RemovedParticleWeight=0.0;
+    int ii;
+    
+    for (ii=0;ii<nDeleteParticles;ii++) {
+      const bool _search_continue=true;
+      const bool _search_completed=false; 
+
+      bool flag=_search_continue;
+      int ip, cnt=0;
+
+      while ((++cnt<1000000)&&(flag==_search_continue)) {
+        ip=(int)(rnd()*nModelParticles);
+
+        if (w_min/ParticleList[ip].w>rnd()) { // delete particles with smaller weight  
+          RemovedParticleWeight+=ParticleList[ip].w;
+          PIC::ParticleBuffer::DeleteParticle(ParticleList[ip].p,node->block->FirstCellParticleTable[i+_BLOCK_CELLS_X_*(j+_BLOCK_CELLS_Y_*k)]);
+
+          ParticleList[ip]=ParticleList[nModelParticles-1];
+          nModelParticles--;
+          flag=_search_completed; 
+        }
+      } 
+    }
+    
+
+    //distribute the removed weight among particles that left in the system 
+    for (ii=0;ii<nModelParticles;ii++) {
+      w=ParticleList[ii].w*(1.0+RemovedParticleWeight/(SummedWeight-RemovedParticleWeight));    
+      p=ParticleList[ii].p;
+
+      PIC::ParticleBuffer::SetIndividualStatWeightCorrection(w,p);
+    }
+  }; 
+  
+    
   //collect particles in the cell block 2x2x2
-  auto CollectParticleBlock = [&] (int i0,int j0,int k0,list<cParticleId>& ParticleList) {
+  auto CollectParticleBlock = [&] (int spec,int i0,int j0,int k0,list<cParticleId>& ParticleList) {
     cParticleId t;
     long int p;
     double ParticleWeight;
@@ -349,12 +341,14 @@ void PIC::ParticleSplitting::Split::Scatter(int particle_num_limit_min,int parti
       t.nd=_getCenterNodeLocalNumber(i,j,k);
 
       while (p!=-1) {
-        t.ptr=p; 
-        ParticleList.push_back(t);
-
-        ParticleWeight=PIC::ParticleBuffer::GetIndividualStatWeightCorrection(p);
-        ParticleWeight/=8.0;
-        PIC::ParticleBuffer::SetIndividualStatWeightCorrection(ParticleWeight,p);
+        if (PIC::ParticleBuffer::GetI(p)==spec) {
+          t.ptr=p; 
+          ParticleList.push_back(t);
+  
+          ParticleWeight=PIC::ParticleBuffer::GetIndividualStatWeightCorrection(p);
+          ParticleWeight/=8.0;
+          PIC::ParticleBuffer::SetIndividualStatWeightCorrection(ParticleWeight,p);
+        }
 
         p=PIC::ParticleBuffer::GetNext(p);
       }
@@ -387,10 +381,10 @@ void PIC::ParticleSplitting::Split::Scatter(int particle_num_limit_min,int parti
     node=PIC::DomainBlockDecomposition::BlockTable[inode];
 
     if (node->block!=NULL) {
-      for (i0=0;i0+1<_BLOCK_CELLS_X_;i0+=2) for (j0=0;j0+1<_BLOCK_CELLS_Y_;j0+=2) for (k0=0;k0+1<_BLOCK_CELLS_Z_;k0+=2) {
-        nParticles=GetBlockParticleNumber(i0,j0,k0,node);
+      for (int s=0;s<PIC::nTotalSpecies;s++) for (i0=0;i0+1<_BLOCK_CELLS_X_;i0+=2) for (j0=0;j0+1<_BLOCK_CELLS_Y_;j0+=2) for (k0=0;k0+1<_BLOCK_CELLS_Z_;k0+=2) {
+        nParticles=GetBlockParticleNumber(s,i0,j0,k0,node);
         
-        if (nParticles<particle_num_limit_min*8) { // 8<- the number of cell in the cell block 2x2x2
+        if ((nParticles!=0)&&(nParticles<particle_num_limit_min*8)) { // 8<- the number of cell in the cell block 2x2x2
           //the number of particles is below the limit: scatter the particles 
           list<cParticleId> ParticleList;
           double dx[3],*xmin,x[3];
@@ -401,7 +395,7 @@ void PIC::ParticleSplitting::Split::Scatter(int particle_num_limit_min,int parti
           dx[1]=(node->xmax[1]-xmin[1])/_BLOCK_CELLS_Y_;
           dx[2]=(node->xmax[2]-xmin[2])/_BLOCK_CELLS_Z_;
           
-          CollectParticleBlock(i0,j0,k0,ParticleList);
+          CollectParticleBlock(s,i0,j0,k0,ParticleList);
       
           
           for (di=0;di<2;di++) for (dj=0;dj<2;dj++) for (dk=0;dk<2;dk++) { 
@@ -412,17 +406,14 @@ void PIC::ParticleSplitting::Split::Scatter(int particle_num_limit_min,int parti
             ScatterParticles(i,j,k,xmin,dx,ParticleList);
           }
         }
-        
-        
-        vector<cParticleListElement> ParticleList;
-      
+              
         for (di=0;di<2;di++) for (dj=0;dj<2;dj++) for (dk=0;dk<2;dk++) { 
           i=i0+di;   
           j=j0+dj;
           k=k0+dk; 
           
-          if (GetParticleNumber(i,j,k,node)>particle_num_limit_max) {
-            ReduceParicleNumber(i,j,k,node,ParticleList,particle_num_limit_max);
+          if (GetParticleNumber(s,i,j,k,node)>particle_num_limit_max) {
+            ReduceParticleNumber(s,i,j,k,node,particle_num_limit_max);
           }
         }
       }
