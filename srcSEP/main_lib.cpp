5f614943a50fa8773280d55759ddcf63fe12ecdf
Valeriy Tenishev <vtenishe@pfe23.nas.nasa.gov>
2022-09-01 23:22:50 -0700

    SEP model initialization: the particle buffer is initialized only in the case it was not initialized before

--- srcSEP/main_lib.cpp
+++ srcSEP/main_lib.cpp
@@ -513,7 +513,7 @@ void amps_init() {
   if (PIC::Mesh::mesh->ThisThread==0) cout << "The mesh is generated" << endl;
 
   //init the particle buffer
-  PIC::ParticleBuffer::Init(10000000);
+  if (PIC::ParticleBuffer::ParticleDataBuffer==NULL) PIC::ParticleBuffer::Init(10000000);
 //  double TimeCounter=time(NULL);
 //  int LastDataOutputFileNumber=-1;
 
