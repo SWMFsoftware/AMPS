4a09e1015182ca6fc7c9457a0c8203192af03386
Valeriy M Tenishev <vtenishe@umich.edu>
2023-11-08 01:08:08 -0500

    added: the procedure for populating a field line with particles

--- srcSEP/shock_injection.cpp
+++ srcSEP/shock_injection.cpp
@@ -86,4 +86,70 @@ void SEP::ParticleSource::ShockWave::Output::PrintVariableList(FILE* fout,int Da
   fprintf(fout,", \"Is shock\"");
 }
 
+void SEP::ParticleSource::PopulateAllFieldLines() {
+  namespace FL = PIC::FieldLine;
+  int iFieldLine;
+
+  for (iFieldLine=0;iFieldLine<FL::nFieldLine;iFieldLine++) {
+    PopulateFieldLine(iFieldLine);
+  } 
+}
+
+void SEP::ParticleSource::PopulateFieldLine(int iFieldLine) { 
+  namespace FL = PIC::FieldLine;
+  int iSegment; 
+  FL::cFieldLineSegment* Segment;
+
+
+  for (iSegment=0,Segment=FL::FieldLinesAll[iFieldLine].GetFirstSegment(); iSegment<FL::FieldLinesAll[iFieldLine].GetTotalSegmentNumber(); iSegment++,Segment=Segment->GetNext()) {
+    double t_sw_end,t_sw_begin,v_sw_end[3],v_sw_begin[3],n_sw_end,n_sw_begin,v[3],NumberDensity,Temperature,Volume;
+    auto VertexBegin=Segment->GetBegin();
+    auto VertexEnd=Segment->GetEnd();
+  
+    VertexBegin->GetDatum(FL::DatumAtVertexPlasmaTemperature,&t_sw_begin);
+    VertexBegin->GetDatum(FL::DatumAtVertexPlasmaDensity,&n_sw_begin);
+    VertexBegin->GetPlasmaVelocity(v_sw_begin);
+
+    VertexEnd->GetDatum(FL::DatumAtVertexPlasmaTemperature,&t_sw_end);
+    VertexEnd->GetDatum(FL::DatumAtVertexPlasmaDensity,&n_sw_end);
+    VertexEnd->GetPlasmaVelocity(v_sw_end);
+
+    NumberDensity=0.5*(n_sw_begin+n_sw_end);
+    Temperature=0.5*(t_sw_begin+t_sw_end);
+
+    Volume=SEP::FieldLine::GetSegmentVolume(Segment,iFieldLine);  
+
+    for (int idim=0;idim<3;idim++) v[idim]=0.5*(v_sw_begin[idim]+v_sw_end[idim]);
+
+    PIC::FieldLine::PopulateSegment(_H_PLUS_SPEC_,NumberDensity,Temperature,v,Volume,iSegment,iFieldLine,200);
+ }
+}
+
+//===================================================================================
+//calcualte volume associated with a segment of the field line 
+double SEP::FieldLine::MagneticTubeRadius(double *x,int iFieldLine) {
+  namespace FL = PIC::FieldLine;
+  double *x0;
+
+  //1. the radius of the magnetic tube as the first vertex from the beginnig of the filed line is ONE
+  //2. the radius increases as R^2 
+  x0=FL::FieldLinesAll[iFieldLine].GetFirstSegment()->GetBegin()->GetX(); 
+
+  return Vector3D::DotProduct(x,x)/Vector3D::DotProduct(x0,x0);
+}
+
+double SEP::FieldLine::MagneticTubeRadius(PIC::FieldLine::cFieldLineVertex* Vertex,int iFieldLine) {
+  return MagneticTubeRadius(Vertex->GetX(),iFieldLine);
+}
+
+double SEP::FieldLine::GetSegmentVolume(PIC::FieldLine::cFieldLineSegment* Segment,int iFieldLine) {
+  double r0,r1;
+
+  r0=MagneticTubeRadius(Segment->GetBegin(),iFieldLine);
+  r1=MagneticTubeRadius(Segment->GetEnd(),iFieldLine);
+
+  return Pi*(r0*r0+r0*r1+r1*r1)*Segment->GetLength()/3.0;
+}
+ 
+
 
