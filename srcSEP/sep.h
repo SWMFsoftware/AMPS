a9e52855be5e48ce5bd897bb3e6ecf0c9a463d7a
Valeriy Tenishev <vtenishe@pfe22.nas.nasa.gov>
2025-06-12 07:49:15 -0700

    declared: S_pm

--- srcSEP/sep.h
+++ srcSEP/sep.h
@@ -154,7 +154,7 @@ namespace SEP {
     namespace IsotropicDistributionSEP {
       void SampleParticleData(double s_new,double s_init,double speed,long int ptr,double dt,PIC::FieldLine::cFieldLineSegment *segment_start,int iFieldLine);
 
-      extern PIC::Datum::cDatumStored S;
+      extern PIC::Datum::cDatumStored S,S_pm;
       const int n_stream_intervals=20;
       extern double log_p_stream_min,log_p_stream_max,log_dp_stream;
     }
