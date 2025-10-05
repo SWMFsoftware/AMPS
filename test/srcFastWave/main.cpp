a8d2af9892c3fbc6264f11e92da981c10ee38365
Valeriy Tenishev <vtenishe@pfe22.nas.nasa.gov>
2025-10-05 06:20:43 -0700

    test_fast-wave: SetIC() is fixed

--- test/srcFastWave/main.cpp
+++ test/srcFastWave/main.cpp
@@ -382,7 +382,7 @@ void SetIC() {
       double dx[3];
       double *xminBlock= node->xmin, *xmaxBlock= node->xmax;
 
-      for (int idim=0;idim<3;idim++) dx[idim]=(xmaxBlock[0]-xminBlock[0])/nBlocks[idim];
+      for (int idim=0;idim<3;idim++) dx[idim]=(xmaxBlock[idim]-xminBlock[idim])/nBlocks[idim];
       
      
       for (k=0;k<_BLOCK_CELLS_Z_;k++) for (j=0;j<_BLOCK_CELLS_Y_;j++) for (i=0;i<_BLOCK_CELLS_X_;i++) {
