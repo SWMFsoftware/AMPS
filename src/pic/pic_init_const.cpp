cee21cb2611414d25f7a600c7d4c5f3df8667da1
Valeriy Tenishev <vtenishe@pfe25.nas.nasa.gov>
2026-01-12 02:09:05 -0800

    pic_init_const.cpp -- rolled back to the privious version

--- src/pic/pic_init_const.cpp
+++ src/pic/pic_init_const.cpp
@@ -81,5 +81,3 @@ int PIC::Sampling::SkipOutputStep=1;
 //switch to temporary disable/enable sampling procedure
 bool PIC::Sampling::RuntimeSamplingSwitch=true;
 
-//conversion factors between normalized and SI units 
-picunits::Factors PIC::Units::Factors;
